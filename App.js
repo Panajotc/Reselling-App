066a2659c8794632103334e8e743908fc5e3eb5c
Panajotc <panajot7@gmail.com>
2023-10-18 16:20:33 +0200

    Loading screen

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { AntDesign } from "@expo/vector-icons";
 import jwtDecode from "jwt-decode";
+// import { AppLoading } from "expo";
+import AppLoading from "expo-app-loading";
 
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import navigationTheme from "./app/assets/Navigation/navigationTheme";
@@ -13,70 +15,9 @@ import AuthNavigator from "./app/assets/Navigation/AuthNavigator";
 import AuthContext from "./app/assets/auth/context";
 import authStorage from "./app/assets/auth/storage";
 
-const Link = () => {
-  const navigation = useNavigation();
-
-  return (
-    <Button
-      title="Click"
-      onPress={() => navigation.navigate("TweetDetails", { id: 1 })}
-    />
-  );
-};
-
-const Tweets = ({ navigation }) => (
-  <Screen>
-    <Text>Tweets</Text>
-    <Link />
-  </Screen>
-);
-
-const TweetDetails = ({ route }) => (
-  <Screen>
-    <Text>Tweet Details {route.params.id}</Text>
-  </Screen>
-);
-
-const Stack = createNativeStackNavigator();
-const FeedNavigator = () => (
-  <Stack.Navigator initialRouteName="Tweets">
-    <Stack.Screen name="Tweets" component={Tweets} />
-    <Stack.Screen name="TweetDetails" component={TweetDetails} />
-  </Stack.Navigator>
-);
-
-const Account = () => (
-  <Screen>
-    <Text>Account</Text>
-  </Screen>
-);
-
-const Tab = createBottomTabNavigator();
-const TabNavigator = () => (
-  <Tab.Navigator
-    tabBarOptions={{
-      activeBackgroundColor: "tomato",
-      activeTintColor: "white",
-      inactiveBackgroundColor: "#eee",
-      inactiveTintColor: "black",
-    }}
-  >
-    <Tab.Screen
-      name="Feed"
-      component={FeedNavigator}
-      options={{
-        headerShown: false,
-        tabBarIcon: ({ size, color }) => (
-          <AntDesign name="home" size={size} color={color} />
-        ),
-      }}
-    />
-    <Tab.Screen name="Account" component={Account} />
-  </Tab.Navigator>
-);
-
 const App = () => {
   const [user, setUser] = useState();
+  const [isReady, setIsReady] = useState(false);
 
   const restoreToken = async () => {
     const token = await authStorage.getToken();
@@ -84,9 +25,14 @@ const App = () => {
     setUser(jwtDecode(token));
   };
 
-  useEffect(() => {
-    restoreToken();
-  }, []);
+  if (!isReady)
+    return (
+      <AppLoading
+        startAsync={restoreToken}
+        onFinish={() => setIsReady(true)}
+        onError={(error) => console.error("App loading error:", error)}
+      />
+    );
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
